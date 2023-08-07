684c002115a60d768af3e3d52e1c5a8f6095316d
meemdarin <meemdarin28@gmail.com>
2023-08-08 01:43:59 +0700

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import PlantFormIconList from "./PlantFormIconList";
 
 interface Props {
   game: Game;
@@ -11,6 +11,9 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlantFormIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
